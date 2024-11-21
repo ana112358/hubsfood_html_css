b39c61ac5619c06e0d351787149c7a1587793acc
camila-braga <milabragalima@hotmail.com>
2024-11-21 17:59:55 -0300

    pag hubs e conserto do carrossel

--- script-carousel.js
+++ script-carousel.js
@@ -1,19 +1,26 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      items: 4,              // Número de itens visíveis por vez
-      loop: true,            // Loop contínuo
-      margin: 10,            // Espaçamento entre itens
-      nav: false,             // Desativando os controles padrão
-      dots: true,
-      autoplay: true,        // Ativa o autoplay
-      autoplayTimeout: 3000  // Intervalo entre transições
-    });
 
-    $(".custom-prev").click(function() {
-        $(".owl-carousel").trigger('prev.owl.carousel');
-      });
-    
-      $(".custom-next").click(function() {
-        $(".owl-carousel").trigger('next.owl.carousel');
-      });
+const TOTAL_CAROUSELS = 100;
+
+$(document).ready(function(){
+
+  $(".owl-carousel").owlCarousel({
+    items: 4,              // Número de itens visíveis por vez
+    loop: true,            // Loop contínuo
+    margin: 10,            // Espaçamento entre itens
+    nav: false,             // Desativando os controles padrão
+    dots: true,
+    autoplay: true,        // Ativa o autoplay
+    autoplayTimeout: 3000  // Intervalo entre transições
   });
+
+  for (let i = 1; i <= TOTAL_CAROUSELS; i++) {
+      $(`#carousel${i}-prev`).click(function() {
+        $(`#carousel${i}`).trigger('prev.owl.carousel');
+      });
+
+      $(`#carousel${i}-next`).click(function() {
+        $(`#carousel${i}`).trigger('next.owl.carousel');
+      });
+    }
+    
+});
