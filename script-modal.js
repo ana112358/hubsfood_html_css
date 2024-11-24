9ec09973a4db06c38e87eb1f7f19c52d28acc5e9
Ana Paula Oliveira de Lima <130050929+ana112358@users.noreply.github.com>
2024-11-24 12:01:10 -0300

    feat: implementei modal de cadastro com opções de Consumidor(a) ou Empresa

--- script-modal.js
+++ script-modal.js
@@ -1,21 +1,38 @@
 // Selecionando elementos do DOM
-const modal = document.getElementById('modal');
+const modalLogin = document.getElementById('modal'); // Modal de login
+const modalCadastro = document.getElementById('modal-cadastro'); // Modal de cadastro
 const loginBtn = document.getElementById('loginBtn');
-const closeModalBtn = document.querySelector('.close-btn');
+const closeModalLoginBtn = modalLogin.querySelector('.close-btn'); // Botão para fechar o modal de login
+const closeModalCadastroBtn = modalCadastro.querySelector('.close-btn'); // Botão para fechar o modal de cadastro
+const btnCriarConta = document.getElementById('criarConta'); // Botão para abrir o modal de cadastro
 
 // Função para abrir o modal ao clicar no botão "Logar"
 loginBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
+    modalLogin.style.display = 'flex';
+});
+// Função para abrir o modal de cadastro ao clicar no botão "Criar Nova conta"
+btnCriarConta.addEventListener('click', () => {
+    modalLogin.style.display = 'none'; // Fecha o modal de login
+    modalCadastro.style.display = 'flex'; // Abre o modal de cadastro
 });
 
 // Função para fechar o modal
-closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+closeModalLoginBtn.addEventListener('click', () => {
+    modalLogin.style.display = 'none';
+});
+
+
+// Função para fechar o modal de cadastro
+closeModalCadastroBtn.addEventListener('click', () => {
+    modalCadastro.style.display = 'none';
 });
 
 // Fechar o modal clicando fora do conteúdo
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
-        modal.style.display = 'none';
+        modalLogin.style.display = 'none';
+    }
+    if (event.target === modalCadastro) {
+        modalCadastro.style.display = 'none';
     }
 });
